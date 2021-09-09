656df6e2f3aa0d2212203c4c449609908e2f02bf
ikrom97 <fleck97rgb@gmail.com>
2021-09-10 04:13:39 +0500

    Добавлен новый тип

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 // Category представляет собой категорию, в которой был совершён платёж (авто, аптеки, рестораны и т.д.).
 type Category string
 
+// Status представляет собой статус платежа.
+type Status string
+
+// Предопределённые статусы платежей.
+const (
+	StatusOK       Status = "OK"
+	StatusFail     Status = "FAIL"
+	StatusProgress Status = "INPROGRESS"
+)
+
 // Payment представляет информацию о платеже.
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
